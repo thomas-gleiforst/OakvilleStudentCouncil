61c864165d0c215043a265ae4926f52f0fdae1b0
thomas-gleiforst <ps2dude67@gmail.com>
2020-12-04 23:00:54 -0600

    Changed color scheme

--- FrontEnd/constants/Colors.ts
+++ FrontEnd/constants/Colors.ts
@@ -1,19 +1,19 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#FFB61D";
+const tintColorDark = "#FFB61D";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#fff",
+    background: "#FFB61D",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#FFB61D",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
   },
 };
