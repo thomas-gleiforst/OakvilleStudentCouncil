ed299754fee71809d9f2818de552ec982eeef820
thomas-gleiforst <ps2dude67@gmail.com>
2020-12-04 23:03:24 -0600

    Implemented beginning of main screen

--- FrontEnd/screens/MainScreen.tsx
+++ FrontEnd/screens/MainScreen.tsx
@@ -1,15 +1,52 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { Pressable, StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
 
 export default function Main() {
+  var username: string = "Thomas";
+  var events = [
+    {
+      title: "General Meeting 1",
+      date: Date.now(),
+      room: "204",
+      location: "Oakville High School",
+    },
+    {
+      title: "Volunteer at OMS",
+      date: Date.now() + 5000,
+      room: "205",
+      location: "Oakville High School",
+    },
+  ];
+  var points = 6;
+  var pointGoal = 100;
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Main Screen</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/MainScreen.js" />
+      <Text style={styles.title}>Welcome {username}.</Text>
+      <Text style={styles.title}>What would you like to do?</Text>
+      <Pressable>
+        <View style={styles.event}>
+          <Text style={styles.eventTitle}>Scan QR code</Text>
+        </View>
+      </Pressable>
+      <Text style={styles.title}>Here are your next events.</Text>
+      {events.map((event) => {
+        return (
+          <Pressable>
+            <View style={styles.event}>
+              <Text style={styles.eventTitle}>{event.title}</Text>
+              <Text style={styles.eventDetails}>
+                {event.date.toLocaleString()}
+              </Text>
+              <Text style={styles.eventDetails}>Room: {event.room}</Text>
+              <Text style={styles.eventDetails}>{event.location}</Text>
+            </View>
+          </Pressable>
+        );
+      })}
+      <Text style={styles.link}>Open Calendar</Text>
     </View>
   );
 }
@@ -17,16 +54,36 @@ export default function Main() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
+  event: {
+    backgroundColor: "white",
+    borderRadius: 15,
+    margin: 10,
+  },
+  eventDetails: {
+    color: "#FFB61D",
+    marginLeft: "5px",
+    marginRight: "5px",
+    marginBottom: "2.5px",
+  },
+  eventTitle: {
+    color: "#FFB61D",
+    fontSize: 20,
+    fontWeight: "bold",
+    margin: "5px",
+  },
+  link: {
+    textDecorationLine: "underline",
+  }
 });
